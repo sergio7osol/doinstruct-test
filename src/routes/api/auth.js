f7a035cf6cac686bbef41f011887a5afda25d561
Sergey Osokin <sergey.osokin@netfire.eu>
2024-04-24 18:32:14 +0200

    Created functions for server communication for components.

--- src/routes/api/auth.js
+++ src/routes/api/auth.js
@@ -3,9 +3,6 @@ import { xApiKey, email, password } from './authData';
 export async function getAuthToken() {
 	try {
 		const token = localStorage.getItem('token');
-		const expiresIn = localStorage.getItem('expiresIn');
-
-		console.log('expiresIn: ', expiresIn);
 
 		if (!token) {
 			throw new Error('No token found. Get it first.');
@@ -26,14 +23,42 @@ export async function getAuthToken() {
 			throw new Error('Failed to get a token.');
 		}
 
-		const { token, expiresIn } = await response.json();
+		const { token, expiresIn, refreshToken } = await response.json();
 
 		localStorage.setItem('token', token);
 		localStorage.setItem('expiresIn', expiresIn);
+		localStorage.setItem('refreshToken', refreshToken);
+
+		return token;
 	}
 }
 
-export async function clearToken() {
-	localStorage.removeItem('token');
-	localStorage.removeItem('expiresIn');
+export async function signOut() {
+	try {
+		const refreshToken = localStorage.getItem('refreshToken');
+
+		if (!refreshToken) {
+			console.error('Could not retrieve refreshToken');
+			return;
+		}
+
+		const response = await fetch('/api/auth/sign-out', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'x-api-key': xApiKey
+			},
+			body: JSON.stringify({ refreshToken })
+		});
+		
+		if (!response.ok) {
+			throw new Error('Failed to sign out.');
+		}
+
+		localStorage.removeItem('token');
+		localStorage.removeItem('expiresIn');
+		localStorage.removeItem('refreshToken');
+	} catch (error) {
+		console.error('Error signing out:', error);
+	}
 }
--- src/routes/api/employees.js
+++ src/routes/api/employees.js
@@ -1,6 +1,6 @@
 import { xApiKey } from './authData';
 import { getAuthToken } from './auth';
-import { generateRandomEmployee, generateRandomEmployees } from '/src/lib/data/employees/utils';
+import { generateRandomEmployee } from '/src/lib/data/employees/utils';
 
 async function getEmployeeData() {
 	const token = await getAuthToken();
@@ -34,7 +34,30 @@ async function getEmployeeData() {
 	}
 }
 
-async function employeesUpdate() {
+async function getEmployeesData(count) {
+	const results = [];
+	const promises = [];
+
+	for (let i = 0; i < count; i++) {
+		promises.push(getEmployeeData());
+	}
+
+	const allUpdateResults = await Promise.all(promises)
+		.then((data) => {
+			data.forEach((result) => {
+				results.push(result);
+			});
+
+			return results;
+		})
+		.catch((error) => {
+			console.error('Error:', error);
+		});
+
+	return allUpdateResults;
+}
+
+async function employeesUpdate(count) {
 	const token = await getAuthToken();
 
 	if (!token) {
@@ -42,17 +65,17 @@ async function employeesUpdate() {
 		return;
 	}
 
-	const employee = generateRandomEmployee();
+	const employeesData = await getEmployeesData(count);
 
 	try {
-		const response = await fetch('/api/employees', {
+		const response = await fetch('/api/employees/bulk/import', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
 				Authorization: token,
 				'x-api-key': xApiKey
 			},
-			body: JSON.stringify(employee)
+			body: JSON.stringify({ rows: employeesData })
 		});
 
 		if (!response.ok) {
@@ -66,11 +89,71 @@ async function employeesUpdate() {
 	}
 }
 
-// // Handle the responses
-// responses.forEach((response, index) => {
-// 	if (response) {
-// 		// Do something with the response
-// 	} else {
-// 		// Handle failed requests
-// 	}
-// });
+export async function getProcessingStatus(count) {
+	const token = await getAuthToken();
+
+	if (!token) {
+		console.error('No token found.');
+		return;
+	}
+
+	async function getStatusCompleted(id) {
+		const response = await fetch(`/api/employees/bulk/${id}`, {
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': token,
+				'x-api-key': xApiKey
+			}
+		});
+
+		if (!response.ok) {
+			throw new Error('Failed to fetch Bulk Status');
+		}
+
+		const processData = await response.json();
+		return processData;
+	}
+
+	try {
+		const { id, href } = await employeesUpdate(count);
+
+		if (!id || !href) {
+			console.error('Failed getting processing status.');
+			return;
+		}
+
+		return getStatusCompleted(id);
+	} catch (error) {
+		console.error('Error fetching employees:', error);
+	}
+}
+
+export async function getAllEmployees() {
+	const token = await getAuthToken();
+
+	if (!token) {
+		console.error('No token found.');
+		return;
+	}
+
+	try {
+		const response = await fetch('/api/employees?limit=10&offset=0', {
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': token,
+				'x-api-key': xApiKey
+			}
+		});
+
+		if (!response.ok) {
+			throw new Error('Failed to fetch protected data');
+		}
+
+		const data = await new Promise((res) => setTimeout(() => res(response.json()), 2000));
+		const rowsData = data?.rows || [];
+
+		return rowsData;
+	} catch (error) {
+		console.error('Error fetching employees:', error);
+	}
+}
